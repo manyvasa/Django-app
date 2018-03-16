e75f24ac19d58b048abd576e0dd44eb4b97a5265
manyvasa <manyvasa29@gmail.com>
2018-03-16 18:26:40 +0500

    responsive menu-jQeury

--- blog/static/js/btn_hamburger.js
+++ blog/static/js/btn_hamburger.js
@@ -1,7 +1,7 @@
-var btn_hamburger = document.querySelector(".header-menu__icon");
-var layout__viewport = document.querySelector(".layout__viewport");
 
-
-btn_hamburger.onclick = function() {
-    layout__viewport.className = "layout__viewport layout__viewport--pushed";
-}
+ $(document).ready(function(){
+	$('.header-menu__btn').click(function () {
+	    $(this).toggleClass("open")
+		$('.layout__viewport').toggleClass( "layout__viewport--pushed" );
+		});
+	});
